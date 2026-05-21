b3177dbef960a28336db4fe63fdc2b6c45a6a8b9
liviarivamonti-stack <liviarivamonti@gmail.com>
2026-05-21 12:00:03 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,2 @@
-{
-  "name": "realize-api",
-  "version": "1.0.0",
-  "type": "commonjs",
-  "dependencies": {
-    "express": "^4.18.2",
-    "cors": "^2.8.5",
-    "cookie-parser": "^1.4.6",
-    "pg": "^8.11.0"
-  }
-}
+const app = require('../artifacts/api-server/src/app.ts');
+module.exports = app;
